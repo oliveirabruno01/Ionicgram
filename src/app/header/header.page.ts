33d20dfdd73a596121b1ffe952b1ffceb8651dfb
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-07-18 18:12:20 -0300

    feat(header): fill current page icon

--- src/app/header/header.page.ts
+++ src/app/header/header.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['header.page.scss']
 })
 export class HeaderPage {
+  constructor(private router: Router) {}
 
-  constructor() {}
+  getPath() {
+    return this.router.url.split('/')[2]
+  }
 
 }
