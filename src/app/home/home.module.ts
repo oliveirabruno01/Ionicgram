f2cdf589d69da8a5c70391d79d3f1b8581778a21
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-07-20 01:40:50 -0300

    feat(home): add post component with profile info

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SwiperModule } from 'swiper/angular';
 import { HomePage } from './home.page';
+import { PostComponent } from '../post/post.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -13,8 +14,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     HomePageRoutingModule,
-    SwiperModule,
+    SwiperModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, PostComponent]
 })
 export class HomePageModule {}
