ce2e3159b504099eaa54e3e6b325a805a01a0691
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-07-22 19:17:44 -0300

    feat(login): add dynamic example images

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { IonImg } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  images: string[] = ['one.png', 'two.png', 'three.png', 'four.png'];
+  image_index = 0;
+  image = '../../assets/start_imgs/two.png';
+  parallaxIsHidden = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    setInterval(()=> {
+      setTimeout(() => {
+        this.parallaxIsHidden = true;
+        this.image = '../../assets/start_imgs/' + this.nextImage();
+      }, 300)
+      this.parallaxIsHidden = false;
+    }, 4000)
+
+  }
+
+  nextImage(): string {
+    if (this.image_index == 0) {
+      this.image_index++;
+      return this.images[0];
+    } else if (this.image_index == this.images.length-1) {
+      this.image_index = 0;
+      return this.images[this.images.length-1];
+    } else {
+      this.image_index++;
+      return this.images[this.image_index-1];
+    }
   }
 
 }
