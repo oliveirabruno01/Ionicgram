04eb50d936d6d090f1462f3eadc118f859cea390
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-07-29 01:45:38 -0300

    feat(chat): add personal chat view for desktop

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ChatPageRoutingModule } from './chat-routing.module';
 
 import { ChatPage } from './chat.page';
+import { HeaderModule } from '../header/header.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChatPageRoutingModule
+    ChatPageRoutingModule,
+    HeaderModule
   ],
   declarations: [ChatPage]
 })
