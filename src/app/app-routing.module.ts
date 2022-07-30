25ae2b87773dfedd419645559317e21a818dd032
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-07-29 22:28:59 -0300

    feat(profile): create profile page and set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,11 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: ':account',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+
 
 
 ];
