2dbb4248868e215a9988592c1107db4aa53a2465
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-08-03 19:37:27 -0300

    feat(server): make migration

--- src/app/types/User.ts
+++ src/app/types/User.ts
@@ -1,5 +1,8 @@
 export interface User {
-    "id": number;
-    "name": string;
-    "profile_image": string;
+    id: number;
+    name: string;
+    profile_image: string;
+    followers: number;
+    following: number;
+    bio: string;
 }
