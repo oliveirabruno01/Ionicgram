7bb205e0ff8ecd1514ec37281b9858b579fd02f1
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-08-04 22:13:43 -0300

    fix(api): fix explore service misspelled words

--- src/app/api/explore/explore.service.ts
+++ src/app/api/explore/explore.service.ts
@@ -6,23 +6,23 @@ import { ExplorePost } from 'src/app/types/ExplorePost';
   providedIn: 'root'
 })
 export class PostService {
-  private API_URL = 'https://raw.githubusercontent.com/oliveirabruno01/Ionicgram/master/fake-server/posts.json';
+  private API_URL = 'https://raw.githubusercontent.com/oliveirabruno01/Ionicgram/master/fake-server/explore.json';
   
   constructor(private http: HttpClient) { }
 
   getPosts(): Array<ExplorePost> {
     let res = this.http.get<Array<ExplorePost>>(this.API_URL);
-    let posts: Array<ExplorePost> = [];
+    let explore_posts: Array<ExplorePost> = [];
 
     res.subscribe(response => {
-      response.map(post => {
+      response.map(explore_post => {
         let p: ExplorePost = {} as ExplorePost;
-        p.id = post.id;
-        p.author_id = post.author_id;
-        p.image = post.image;
-        posts.push(p);
+        p.id = explore_post.id;
+        p.author_id = explore_post.author_id;
+        p.image = explore_post.image;
+        explore_posts.push(p);
       })
     })
-    return posts;
+    return explore_posts;
   }
 }
