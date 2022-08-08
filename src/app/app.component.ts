0817b48af40e8d253df8762ac24f2ee2fac9c857
oliveirabruno01m <brunocabeludo321@gmail.com>
2022-08-07 21:45:20 -0300

    feat(app): add a refresher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+
+  doRefresh(event) {
+    setTimeout(() => {
+      event.target.complete();
+      window.location.reload();
+    }, 2000);
+  }
 }
